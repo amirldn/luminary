f8bbefda49d9eb19f4829579f88c1204fa10e3e1
amirldn <amirmaula@outlook.com>
2023-10-12 14:33:31 +0100

    got album cover caching working and converting to bmp

--- host.py
+++ host.py
@@ -2,13 +2,15 @@ from PIL import Image
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
+import requests
 import time
+import os
 
 
 class currentPlaying:
     def f(self):
         self.update()
-        return
+        return self
 
     def update(self):
         currentPlaying = sp.current_user_playing_track()
@@ -17,17 +19,56 @@ class currentPlaying:
         if currentPlaying == "None":
             print("Nothing Playing")
             return None
+
+        # Playtime
         timeMs = int(currentPlaying["progress_ms"])
         self.timeSec = int((timeMs / 1000) % 60)
         if len(str(self.timeSec)) == 1:
             self.timeSec = "0" + str(self.timeSec)
         self.timeMin = int((timeMs / (1000 * 60)) % 60)
+
+        # Song Info
         self.title = currentPlaying["item"]["name"]
         self.artist = currentPlaying["item"]["artists"][0]["name"]
-        self.cover_art = currentPlaying["item"]["album"]["images"][2][
-            "url"
-        ]  # 64x64, [0] for higher 300x300
+        # 64x64, [0] for higher 300x300
+        self.cover_art = currentPlaying["item"]["album"]["images"][2]["url"]
+        self.album_id = currentPlaying["item"]["album"]["uri"].split(":")[2]
+
         return self
+
+
+# Image Handling
+cover_art_path = "cover_art/"
+bmp_cover_art_path = "cover_art/bmp/"
+
+if not os.path.exists(cover_art_path):
+    os.makedirs(cover_art_path)
+if not os.path.exists(bmp_cover_art_path):
+    os.makedirs(bmp_cover_art_path)
+
+
+def download_cover_art_and_convert_to_32x32_bmp(url, filename):
+    if not os.path.exists(cover_art_path + filename + ".jpg"):
+        download_cover_art(url, filename)
+        convert_cover_art_to_32x32_bmp(filename)
+        print("Downloaded cover art: " + filename)
+    # else:
+    #     print("File already exists: " + filename)
+
+
+def download_cover_art(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(cover_art_path + filename + ".jpg", "wb") as f:
+            f.write(response.content)
+    else:
+        print("Error downloading cover art: " + response)
+
+
+def convert_cover_art_to_32x32_bmp(filename):
+    img = Image.open(cover_art_path + filename + ".jpg")
+    img = img.resize((32, 32))
+    img.save(bmp_cover_art_path + filename + ".bmp")
 
 
 # Take from creds.json file in same directory
@@ -49,8 +90,24 @@ sp = spotipy.Spotify(
 
 
 cp = currentPlaying()
-# while True:
-if cp.update() != None:
-    print(cp.cover_art)
-    print(str(cp.timeMin) + ":" + str(cp.timeSec) + " " + cp.title + " " + cp.artist)
-    # time.sleep(1)
+while True:
+    now_playing = cp.update()
+    if now_playing != None:
+        # print(now_playing.cover_art)
+        print(
+            str(now_playing.timeMin)
+            + ":"
+            + str(now_playing.timeSec)
+            + " "
+            + now_playing.title
+            + " "
+            + now_playing.artist
+        )
+        download_cover_art_and_convert_to_32x32_bmp(
+            now_playing.cover_art,
+            now_playing.album_id,
+        )
+        time.sleep(1)
+    else:
+        print("Nothing Playing")
+        time.sleep(1)
